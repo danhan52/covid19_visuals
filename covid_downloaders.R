09156dbad51df3b7978ab8f60ca8695ffcc1ab94
danhan52 <danny.hanson52@gmail.com>
2020-06-18 21:41:39 -0500

    Finally get data downloaded and shiny app running

--- covid_downloaders.R
+++ covid_downloaders.R
@@ -1,49 +1,188 @@
 library(tidyverse)
 library(lubridate)
-# setwd('C:/Users/danny/repos/covid19_visuals/')
+library(rvest)
+setwd('C:/Users/danny/repos/covid19_visuals/')
 
-download_date <- function(dte = today() - 1, type = c('global', 'us')) {
-  # from https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data
-  print(dte)
-  type <- match.arg(type)
-  url_base <-
-    switch (type,
-            'global' = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/',
-            'us' = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/'
-            # 'ts' = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/'
-    )
-  
-  dt <- format(dte, '%m-%d-%Y')
-  df <- 
-    url(paste0(url_base, dt, '.csv')) %>%
-    read_csv()
-  
-  names(df) <- str_replace_all(names(df), '\\/|\\ ', '_')
-  if ('FIPS' %in% names(df)) {
-    df <- df %>% mutate(FIPS = as.numeric(FIPS))
-  }
-  
-  df <- 
-    df %>%
-    mutate(
-      dwnld_date = dte,
-      Last_Update = as.POSIXct(
-        Last_Update,
-        tryFormats = c("%Y-%m-%d %H:%M:%OS",
-                       "%Y/%m/%d %H:%M:%OS",
-                       "%Y-%m-%d %H:%M",
-                       "%Y/%m/%d %H:%M",
-                       "%Y-%m-%d",
-                       "%Y/%m/%d",
-                       "%m/%d/%Y %H:%M",
-                       "%m/%d/%y %H:%M"))
-    )
-  df
-}
+# US COVID-19 data ####
+# https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/
+covid_us <- 
+  c('confirmed' = 'confirmed', 'deaths' = 'deaths') %>%
+  map(
+    function(x) {
+      paste0(
+        'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/',
+        'master/csse_covid_19_data/csse_covid_19_time_series/',
+        'time_series_covid19_',
+        x,
+        '_US.csv'
+      )
+    }
+  ) %>%
+  map_df(function(x) read_csv(url(x)), .id = 'metric') %>%
+  pivot_longer(cols = `1/22/20`:last_col(),
+               names_to = 'date',
+               values_to = 'people') %>%
+  mutate(
+    fipstr = str_pad(FIPS, 5, 'left', '0'),
+    date = as.Date(date, format = '%m/%d/%y')
+  )
 
-# min_d <- date('2020-01-22')
-# max_d <- today() - 1
-# df <- map_df(seq(min_d, max_d, length.out = max_d - min_d),
-#              download_date, type = 'global')
-# names(df) <- tolower(names(df))
-# write_csv(df, paste0('data/global_covid19_through_', max_d, '.csv'))
+# https://www.census.gov/data/datasets/time-series/demo/popest/2010s-counties-total.html#par_textimage_70769902
+us_pop <-
+  read_csv(url(paste0('https://www2.census.gov/programs-surveys/popest/',
+                      'datasets/2010-2019/counties/totals/',
+                      'co-est2019-alldata.csv'))) %>%
+  rename_all(tolower) %>%
+  select(stname, ctyname, popestimate2019, state, county) %>%
+  mutate(
+    ctyname = str_replace(ctyname, ' County', ''),
+    fipstr = paste0(state, county)
+  )
+
+covid_us <-
+  covid_us %>%
+  left_join(us_pop, by = 'fipstr')
+
+
+# Global COVID-19 data ####
+# https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/
+covid_global <- 
+  c(confirmed = 'confirmed', deaths = 'deaths') %>%
+  map(
+    function(x) {
+      paste0(
+        'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/',
+        'master/csse_covid_19_data/csse_covid_19_time_series/',
+        'time_series_covid19_',
+        x,
+        '_global.csv'
+      )
+    }
+  ) %>%
+  map_df(function(x) read_csv(url(x)), .id = 'metric') %>%
+  pivot_longer(cols = `1/22/20`:last_col(),
+               names_to = 'date',
+               values_to = 'people') %>%
+  mutate(date = as.Date(date, format = '%m/%d/%y'))
+
+# https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1710000901
+canada_pop <-
+  read_csv(url(paste0(
+    'https://www150.statcan.gc.ca/t1/tbl1/en/dtl!downloadDbLoadingData',
+    '-nonTraduit.action?pid=1710000901&latestN=5&startDate=&endDate=',
+    '&csvLocale=en&selectedMembers=%5B%5B2%2C8%2C1%2C5%2C3%2C14%2C10%2C9',
+    '%2C7%2C11%2C12%2C4%2C6%2C15%5D%5D'
+  ))) %>%
+  rename_all(tolower) %>%
+  filter(ref_date == '2019-04') %>%
+  select(province = geo, pop = value) %>%
+  mutate(country = 'Canada')
+
+# https://www.worldatlas.com/articles/the-largest-states-and-territories-of-australia.html
+australia_pop <-
+  read_html(
+    'https://www.worldatlas.com/articles/the-largest-states-and-territories-of-australia.html'
+  ) %>%
+  html_nodes('table') %>%
+  html_table() %>%
+  .[[1]] %>%
+  as_tibble() %>%
+  mutate(
+    country = 'Australia',
+    province = `<U+FEFF>State/Territory`,
+    pop = as.numeric(str_remove_all(Population, ','))
+  ) %>%
+  select(province, pop, country)
+
+# https://www.worldatlas.com/articles/chinese-provinces-by-population.html
+china_pop <-
+  read_html('https://www.worldatlas.com/articles/chinese-provinces-by-population.html') %>%
+  html_nodes('table') %>%
+  html_table() %>%
+  .[[1]] %>%
+  as_tibble()
+names(china_pop) <- c('rank', 'province', 'pop')
+china_pop <-
+  china_pop %>%
+  mutate(
+    country = 'China',
+    pop = as.numeric(str_remove_all(pop, ','))
+  ) %>%
+  select(province, pop, country)
+
+# china, canada, australia
+covid_global <-
+  covid_global %>%
+  left_join(
+    bind_rows(china_pop, canada_pop, australia_pop) %>%
+      mutate(province = str_remove(province, '[^[:alnum:]|[:blank:]]')), 
+    by = c('Province/State'='province', 'Country/Region'='country')
+  )
+
+# join and add world population ####
+# https://worldpopulationreview.com/
+world_pop <-
+  read_html('https://worldpopulationreview.com/') %>%
+  html_nodes('table') %>%
+  html_table() %>%
+  .[[1]] %>%
+  as_tibble() %>%
+  mutate(
+    country = Country,
+    cntry_pop = as.numeric(str_remove_all(`2019 Population`, ','))
+  ) %>%
+  select(country, cntry_pop)
+
+covid <-
+  bind_rows(
+    covid_us %>%
+      mutate(Province_State = str_replace(Province_State, 'Virgin Islands',
+                                          'United States Virgin Islands')) %>%
+      select(
+        country = Country_Region, state = Province_State, county = Admin2,
+        lat = Lat, long = Long_, date, metric, people, pop = popestimate2019
+      ),
+    covid_global %>%
+      filter(!`Country/Region` %in% c('US')) %>%
+      mutate(county = NA) %>%
+      select(
+        country = `Country/Region`, state = `Province/State`, county,
+        lat = Lat, long = Long, date, metric, people, pop
+      )
+  ) %>%
+  left_join(world_pop, by = c('state' = 'country')) %>%
+  rename(state_pop = cntry_pop) %>%
+  left_join(world_pop, by = 'country') %>%
+  mutate(
+    actl_pop = case_when(
+      country == 'US' ~ coalesce(pop, state_pop),
+      country %in% c('Canada', 'Australia', 'China') ~ pop,
+      country %in% c('Netherlands', 'UK', 'France', 'Denmark') ~ coalesce(state_pop, cntry_pop),
+      T ~ cntry_pop
+    )
+    # state = coalesce(state, country),
+    # county = coalesce(county, state, country)
+  ) %>%
+  select(-c(pop, state_pop, cntry_pop))
+
+covid_long <-
+  covid %>%
+  mutate(county = if_else(!is.na(county), paste0(county, ' County, ', state), as.character(NA))) %>%
+  pivot_longer(cols = country:county, 
+               names_to = 'loc_type',
+               values_to = 'loc',
+               values_drop_na = TRUE) %>%
+  group_by(date, metric, loc_type, loc) %>%
+  summarize(
+    lat = mean(lat, na.rm = T),
+    long = mean(long, na.rm = T),
+    people = sum(people, na.rm = T),
+    pop = sum(actl_pop, na.rm = T)
+  ) %>%
+  ungroup() %>%
+  filter(!is.na(date))
+
+write_csv(covid_long, 'data/covid_long.csv')
+save(covid_long, file = 'data/covid.RData')
+
+rm(list = setdiff(ls(), c('covid_long')))
--- simple_visualizer/app.R
+++ simple_visualizer/app.R
@@ -2,259 +2,105 @@ library(shiny)
 library(plotly)
 library(tidyverse)
 library(lubridate)
+options(scipen = 100)
 setwd('C:/Users/danny/repos/covid19_visuals/')
 
-covid_global <- 
-    c(confirmed = 'confirmed', deaths = 'deaths', recovered = 'recovered') %>%
-    map(
-        function(x) {
-            paste0(
-                'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/',
-                'master/csse_covid_19_data/csse_covid_19_time_series/',
-                'time_series_covid19_',
-                x,
-                '_global.csv'
-            )
-        }
-    ) %>%
-    map_df(function(x) read_csv(url(x)), .id = 'metric') %>%
-    pivot_longer(cols = `1/22/20`:last_col(),
-                 names_to = 'date',
-                 values_to = 'people') %>%
-    mutate(county = NA) %>%
-    select(
-        country_region = `Country/Region`,
-        province_state = `Province/State`,
-        county,
-        date,
-        metric,
-        people,
-        lat = Lat,
-        long = Long
-    )
-
-
-covid_us <- 
-    c('confirmed' = 'confirmed', 'deaths' = 'deaths') %>%
-    map(
-        function(x) {
-            paste0(
-                'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/',
-                'master/csse_covid_19_data/csse_covid_19_time_series/',
-                'time_series_covid19_',
-                x,
-                '_US.csv'
-            )
-        }
-    ) %>%
-    map_df(function(x) read_csv(url(x)), .id = 'metric') %>%
-    pivot_longer(cols = `1/22/20`:last_col(),
-                 names_to = 'date',
-                 values_to = 'people') %>%
-    select(
-        country_region = `Country_Region`,
-        province_state = `Province_State`,
-        county = Admin2,
-        date,
-        metric,
-        people,
-        lat = Lat,
-        long = Long_
-    )
-
-covid_us %>%
-    left_join
-
-mx_dt <- 
-    dir('data') %>%
-    .[str_detect(., 'global_covid19_through_')] %>%
-    str_replace('global_covid19_through_(.+).csv', '\\1') %>%
-    as_date() %>%
-    max()
-covid <- read_csv(paste0('data/global_covid19_through_', mx_dt, '.csv'), guess_max = 1e6)
-
-if (mx_dt < today() - 1) {
-    min_d <- as.Date(mx_dt)
-    max_d <- as.Date(today() - 1)
-    covid <- bind_rows(
-        covid,
-        map_df(seq(min_d, max_d, length.out = max_d - min_d + 1),
-               download_date, type = 'global')
-    )
-    write_csv(covid, paste0('global_covid19_through_', max_d, '.csv'))
-    rm(min_d, max_d)
-}
-
-
-us_pop <-
-    read_csv('data/us_pop.csv') %>%
-    rename_all(tolower) %>%
-    select(stname, ctyname, popestimate2019) %>%
-    mutate(ctyname = str_replace(ctyname, ' County', ''))
-canada_pop <-
-    read_csv('data/canada_pop.csv') %>%
-    rename_all(tolower) %>%
-    filter(ref_date == '2019-01') %>%
-    select(geo, province_pop = value)
-world_pop <-
-    read_csv('data/world_pop.csv') %>%
-    mutate(world_pop = pop2019 * 1000) %>%
-    select(name, world_pop)
-
-library(tictoc)
-tic()
-covid %>%
-    filter(confirmed > 10 | deaths > 1, !str_detect(province_state, ',')) %>%
-    mutate(
-        country_region = str_replace(country_region, 'US', 'United States'),
-        country_region = str_replace(country_region, 'UK', 'United Kingdom'),
-        country_region = str_replace(country_region, 'Mainland China', 'China'),
-        country_region = str_replace(country_region, 'Republic of Korea', 'North Korea'),
-        country_region = str_replace(country_region, 'Korea, South', 'South Korea'),
-        country_region = str_replace(country_region, 'Iran \\(Islamic Republic of\\)', 'Iran'),
-        country_region = str_replace(country_region, 'Holy See', 'Vatican City'),
-        country_region = str_replace(country_region, 'Russian Federation', 'Russia'),
-        country_region = str_replace(country_region, 'Bahamas, The', 'Bahamas'),
-        country_region = str_replace(country_region, 'The Bahamas', 'Bahamas'),
-        country_region = str_replace(country_region, 'Czechia', 'Czech Republic')
-        # country_region = str_replace(country_region, '', '')
-    ) 
-toc()
-tic()
-covid %>%
-    filter(confirmed > 10 | deaths > 1, !str_detect(province_state, ',')) %>%
-    mutate(
-        country_region = str_replace(
-            country_region, 
-            c('US', 'UK', 'Mainland China', 'Republic of Korea', 'Korea, South', 'Iran \\(Islamic Republic of\\)',
-              'Holy See', 'Russian Federation', 'Bahamas, The', 'The Bahamas', 'Czechia'), 
-            c('United States', 'United Kingdom', 'China', 'North Korea', 'South Korea', 'Iran', 'Vatican City',
-              'Russia', 'Bahamas', 'Bahamas', 'Czech Republic'))
-    ) %>%
-    filter(country_region %in% c('United States', 'United Kingdom', 'China', 'North Korea', 'South Korea', 'Iran', 'Vatican City',
-                                 'Russia', 'Bahamas', 'Bahamas', 'Czech Republic'))
-toc()
-
-covid_pop <-
-    covid %>%
-    filter(confirmed > 10 | deaths > 1, !str_detect(province_state, ',')) %>%
-    mutate(
-        country_region = str_replace(country_region, 'US', 'United States'),
-        country_region = str_replace(country_region, 'UK', 'United Kingdom'),
-        # country_region = str_replace(country_region, 'Mainland China', 'China'),
-        # country_region = str_replace(country_region, 'Republic of Korea', 'North Korea'),
-        # country_region = str_replace(country_region, 'Korea, South', 'South Korea'),
-        # country_region = str_replace(country_region, 'Iran \\(Islamic Republic of\\)', 'Iran'),
-        # country_region = str_replace(country_region, 'Holy See', 'Vatican City'),
-        # country_region = str_replace(country_region, 'Russian Federation', 'Russia'),
-        # country_region = str_replace(country_region, 'Bahamas, The', 'Bahamas'),
-        # country_region = str_replace(country_region, 'The Bahamas', 'Bahamas'),
-        # country_region = str_replace(country_region, 'Czechia', 'Czech Republic')
-        # country_region = str_replace(country_region, '', '')
-    ) %>%
-    left_join(world_pop, by = c('country_region'='name')) %>%
-    left_join(canada_pop, by = c('province_state'='geo'))
-
-covid_pop %>% filter(is.na(world_bank_pop)) %>%
-    group_by(province_state) %>% summarize(dt = sum(deaths)) %>% arrange(desc(dt))
-
-covid_pop %>% filter(country_region == 'Canada', is.na(province_pop)) %>%
-    group_by(province_state) %>% summarize(dt = sum(deaths)) %>% arrange(desc(dt))
-
-plot_state <- function(df, state, by_county = FALSE, metric = 'deaths', h = 600, w = 800) {
-    df_plt <-
-        df %>%
-        filter(province_state %in% state) %>%
-        mutate(state = province_state, county = admin2)
-    if (!by_county) {
-        df_plt <-
-            df_plt %>%
-            mutate(county = state)
-    }
-    df_plt <-
-        df_plt %>%
-        group_by(state, dwnld_date, county) %>%
-        summarize(
-            confirmed = sum(confirmed, na.rm = T),
-            deaths = sum(deaths, na.rm = T),
-            recovered = sum(recovered, na.rm = T)
-        ) %>%
-        ungroup() %>%
-        group_by(state, county) %>%
-        mutate(
-            confirmed_day = confirmed - lag(confirmed, order_by = dwnld_date, default = 0),
-            deaths_day = deaths - lag(deaths, order_by = dwnld_date, default = 0),
-            recovered_day = recovered - lag(recovered, order_by = dwnld_date, default = 0)
-        ) %>%
-        ungroup()
-    
-    p <-
-        df_plt %>%
-        ggplot(aes(x = dwnld_date, group = county, color = state))
-    if (metric == 'deaths') {
-        p <- p + 
-            geom_line(aes(y = deaths)) +
-            geom_bar(aes(y = deaths_day), stat = 'identity')
-    } else if (metric == 'confirmed') {
-        p <- p + 
-            geom_line(aes(y = confirmed)) +
-            geom_bar(aes(y = confirmed_day), stat = 'identity')
-    }
-    ggplotly(p + geom_hline(yintercept = 100), height = h, width = w)
-}
+load('data/covid.RData')
 
 ui <- fluidPage(
-    titlePanel("State Visualizer"),
-    sidebarLayout(
-        sidebarPanel(
-            selectInput(
-                inputId = 'state',
-                label = 'State',
-                choices = sort(unique(covid$province_state)),
-                selected = 'Minnesota',
-                multiple = TRUE
-            ),
-            selectInput(
-                inputId = 'metric', 
-                label = 'Metric',
-                choices = c('deaths', 'confirmed')
-            ),
-            checkboxInput(
-                inputId = 'by_county',
-                label =  'Group by county',
-                value = FALSE
-            ),
-            numericInput(
-                inputId = 'h',
-                label = 'Plot height',
-                value = 600
-            ),
-            numericInput(
-                inputId = 'w',
-                label = 'Plot width',
-                value = 800
-            ),
-            actionButton(
-                inputId = 'refresh',
-                label = 'Refresh COVID data'
-            )
+    title = "COVID-19 Visualizer",
+    plotlyOutput("covidPlot", width = '100%', height = '100%'),
+    
+    hr(),
+    
+    fluidRow(
+        column(4,
+               selectInput(
+                   inputId = 'loc',
+                   label = 'Location (Country, State/Province, County)',
+                   choices = sort(unique(covid_long$loc)),
+                   selected = c('Minnesota', 'Cyprus', 'Japan', 'Quebec'),
+                   multiple = TRUE
+               ),
+               selectInput(
+                   inputId = 'metric', 
+                   label = 'Metric',
+                   choices = c('deaths', 'confirmed'),
+                   selected = 'deaths',
+                   multiple = TRUE
+               )
         ),
-        mainPanel(
-           plotlyOutput("covidPlot", width = '100%', height = '100%')
+        column(4,
+               sliderInput(
+                   inputId = 'st_date',
+                   label = 'Start Date',
+                   min = min(covid_long$date),
+                   max = max(covid_long$date),
+                   value = as.Date('2020-03-15')
+               ),
+               checkboxInput(
+                   inputId = 'percap',
+                   label = 'Per 1000 People'
+               ),
+               checkboxInput(
+                   inputId = 'total',
+                   label = 'Show total line',
+                   value = TRUE
+               ),
+               checkboxInput(
+                   inputId = 'change',
+                   label = 'Show daily change',
+                   value = FALSE
+               )
+        ),
+        column(4,
+               numericInput(
+                   inputId = 'w',
+                   label = 'Plot width',
+                   value = 1200
+               ),
+               numericInput(
+                   inputId = 'h',
+                   label = 'Plot height',
+                   value = 800
+               )
         )
     )
 )
 
 server <- function(input, output) {
     output$covidPlot <- renderPlotly({
-        plot_state(
-            covid,
-            state = input$state,
-            by_county = input$by_county,
-            metric = input$metric,
-            h = input$h,
-            w = input$w
-        )
+        plt_df <- 
+            covid_long %>%
+            filter(
+                loc %in% input$loc,
+                metric %in% input$metric,
+                date >= input$st_date
+            ) %>%
+            group_by(loc, loc_type, metric) %>%
+            mutate(
+                delta = people - lag(people, order_by = date, default = 0),
+                percap = input$percap,
+                people = if_else(percap, people / pop * 1000, people),
+                delta = if_else(percap, delta / pop * 1000, delta)
+            )
+        
+        
+        line_plt <-
+            plt_df %>%
+            ggplot(aes(x = date, color = loc, linetype = metric))
+        if (input$total) {
+            line_plt <-
+                line_plt +
+                geom_line(aes(y = people))
+        }
+        if (input$change) {
+            line_plt <-
+                line_plt +
+                geom_bar(aes(y = delta), stat = 'identity')
+        }
+        
+        ggplotly(line_plt, width = input$w, height = input$h)
     })
 }
 
